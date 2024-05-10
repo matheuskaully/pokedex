18b74751773371abf6d3dfd051f4fe8b8c1f1b87
Matheus Kaúlly <kaully.santos@gmail.com>
2024-05-09 22:55:17 -0300

    feat: add pokemon list

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { PokeApiService } from './../../services/poke-api.service';
+import { Component, OnInit } from '@angular/core';
+import { NgFor, NgForOf, NgTemplateOutlet } from '@angular/common';
 
 @Component({
   selector: 'app-poke-list',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, NgFor, NgForOf, NgTemplateOutlet],
   templateUrl: './poke-list.component.html',
   styleUrl: './poke-list.component.scss'
 })
-export class PokeListComponent {
+export class PokeListComponent implements OnInit {
+ public getAllPokemons: any
 
+ constructor(
+  private pokeApiService: PokeApiService
+ ) {}
+
+ ngOnInit(): void {
+   this.pokeApiService.apiListAllPokemons.subscribe(
+    response => {
+      this.getAllPokemons = response.results
+      console.log(this.getAllPokemons)
+    }
+   )
+ }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'pokemon-bg': "url('/assets/bg/background/list-pokemons.svg')",
+      }
+    },
   },
   plugins: [],
 }
